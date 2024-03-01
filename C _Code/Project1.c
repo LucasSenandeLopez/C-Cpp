35b8fd108f866e6ecb55b272204dee574f822726
LucasSenandeLopez <lsenandelopez03@gmail.com>
2024-03-01 19:55:15 +0100

    struct example by a friend

--- C _Code/Project1.c
+++ C _Code/Project1.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
+typedef struct {
+  long int id;
+  double average_grade;
+  char* name;
+  char* surname;
+  int age;
+} Student; 
+
+double get_grade(const Student* student);
+
 int main(){
 
-    for(int i = 0; i < 10; i++){
-        printf("test\n");
-    }
-    return 0;
+  const Student st_1 = {1, 60, "ab", "bc", 14};  
+
+  printf("avg_grade = %f", get_grade(&st_1));
+  return 0;
+}
+
+double get_grade(const Student* const student){
+  return student->average_grade;
 }
