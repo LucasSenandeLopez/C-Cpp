df23cb4e200f5257bb99c206d581a2b941bc853d
LucasSenandeLopez <lsenandelopez03@gmail.com>
2024-04-06 13:31:16 +0200

    New problems

---
+++ Cpp_Code/IntroductionToCpp/Chapter4/U_4_6/Ex_U_4_6.cpp
@@ -0,0 +1,17 @@
+/*
+
+    We are porposed to write a program that swaps the two quads of an 8 bit integer
+    so we go from 10 (00001010) to 160 (10100000)
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+
+
+    return 0;
+}
